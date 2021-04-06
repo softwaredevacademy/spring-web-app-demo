316b38dd413cc30608767dde96686d59bda1192f
Kwabena <kwabena.a92@gmail.com>
2021-04-07 01:33:08 +0200

    Add put, get, and delete mappings

--- src/main/java/se/sdaproject/Person.java
+++ src/main/java/se/sdaproject/Person.java
@@ -25,6 +25,14 @@ public class Person {
         this.age = age;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
--- src/main/java/se/sdaproject/PersonController.java
+++ src/main/java/se/sdaproject/PersonController.java
@@ -5,8 +5,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.module.ResolutionException;
 import java.util.List;
 
+@RequestMapping("/people")
 @RestController
 public class PersonController {
 
@@ -17,13 +19,34 @@ public class PersonController {
         this.personRepository = personRepository;
     }
 
-    @GetMapping("/people")
+    @GetMapping
     public List<Person> listAllPeople() {
         List<Person> people = personRepository.findAll();
         return people;
     }
 
-    @PostMapping("/people")
+    @GetMapping("/{id}")
+    public ResponseEntity<Person> getPerson(@PathVariable Long id) {
+        Person person = personRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
+        return ResponseEntity.ok(person);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Person> updatePerson(@PathVariable Long id, @RequestBody Person updatedPerson) {
+        personRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
+        updatedPerson.setId(id);
+        Person person = personRepository.save(updatedPerson);
+        return ResponseEntity.ok(person);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Person> deletePerson(@PathVariable Long id) {
+        Person person = personRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
+        personRepository.delete(person);
+        return ResponseEntity.ok(person);
+    }
+
+    @PostMapping
     public ResponseEntity<Person> createPerson(@RequestBody Person person) {
         personRepository.save(person);
         return ResponseEntity.status(HttpStatus.CREATED).body(person);
