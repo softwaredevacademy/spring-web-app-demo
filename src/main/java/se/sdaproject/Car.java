1dc141ccc20303c52f97d766b26ea8b2175ca66d
Kwabena <kwabena.a92@gmail.com>
2021-04-08 10:34:37 +0200

    Add validation and put mapping for Car

--- src/main/java/se/sdaproject/Car.java
+++ src/main/java/se/sdaproject/Car.java
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Car {
@@ -14,13 +16,27 @@ public class Car {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
+    @NotBlank
     private int topSpeed;
+    @Column(nullable = false)
+    @NotBlank
     private String registrationNumber;
 
     @ManyToOne
     @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
     @JsonIdentityReference(alwaysAsId = true)
+    @JoinColumn(nullable = false)
+    @NotNull
     private Person owner;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public int getTopSpeed() {
         return topSpeed;
--- src/main/java/se/sdaproject/CarController.java
+++ src/main/java/se/sdaproject/CarController.java
@@ -5,7 +5,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
 
 @Controller
 public class CarController {
@@ -28,6 +31,16 @@ public class CarController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(car);
+    }
+
+    @PutMapping("/cars/{id}")
+    public ResponseEntity<Car> updateCar(@PathVariable Long id, @Valid @RequestBody Car updatedCar) {
+        Car car = carRepository
+                .findById(id)
+                .orElseThrow(ResourceNotFoundException::new);
+        updatedCar.setId(id);
+        carRepository.save(updatedCar);
+        return ResponseEntity.ok(updatedCar);
     }
 
 }
